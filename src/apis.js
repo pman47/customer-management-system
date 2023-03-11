eaae2c97cda3e46c4ec9b1a84f849f1bc6eba785
Manish Prajapati <manishprajapatim416@gmail.com>
2023-03-11 13:07:30 +0530

    Created Api's and config variables

---
+++ src/apis.js
@@ -0,0 +1,83 @@
+import { API_URL, COLLECTIONS, STATUSES } from "./config";
+const { CUSTOMER } = COLLECTIONS;
+const { SUCCESS, ERROR } = STATUSES;
+
+export async function getCustomers(id) {
+  let url = API_URL + CUSTOMER;
+  if (id) {
+    url += `/${id}`;
+  }
+  return await fetch(url)
+    .then((res) => res.json)
+    .then((response) => ({
+      type: SUCCESS,
+      message: "Successfully Fetched Customers",
+      data: Array.isArray(response) ? response : [response],
+    }))
+    .catch((_) => ({ type: ERROR, message: "Could not Fetch Customers" }));
+}
+
+export async function createCustomer(data) {
+  const { name, address, phone } = data;
+  let url = API_URL + CUSTOMER;
+  if (name && address && phone) {
+    return await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((res) => res.json)
+      .then((response) => ({
+        type: SUCCESS,
+        message: "Successfully Created Customer",
+        data: response,
+      }))
+      .catch((_) => ({ type: ERROR, message: "Could not Create Customer" }));
+  }
+  return { type: ERROR, message: "Pls submit required data" };
+}
+
+export async function editCustomer(data) {
+  if (data.id) {
+    let url = API_URL + CUSTOMER + `/${data.id}`;
+
+    data.name = data.name || "";
+    data.address = data.address || "";
+    data.phone = data.phone || "";
+
+    return await fetch(url, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((res) => res.json)
+      .then((response) => ({
+        type: SUCCESS,
+        message: "Successfully Updated Customer",
+        data: response,
+      }))
+      .catch((_) => ({ type: ERROR, message: "Could not Update Customer" }));
+  }
+  return { type: ERROR, message: "Pls submit required data" };
+}
+
+export async function removeCustomer(id) {
+  if (id) {
+    let url = API_URL + CUSTOMER + `/${id}`;
+    return await fetch(url, {
+      method: "DELETE",
+    })
+      .then((res) => res.json)
+      .then((response) => ({
+        type: SUCCESS,
+        message: "Successfully Removed Customer",
+        data: response,
+      }))
+      .catch((_) => ({ type: ERROR, message: "Could not Remove Customer" }));
+  }
+  return { type: ERROR, message: "Pls submit required data" };
+}
---
+++ src/config.js
@@ -0,0 +1,10 @@
+export const API_URL = "https://64021a7aab6b7399d0b3e3ba.mockapi.io/api/";
+
+export const COLLECTIONS = {
+  CUSTOMER: "customers",
+};
+
+export const STATUSES = {
+  SUCCESS: "Success",
+  ERROR: "Error",
+};
